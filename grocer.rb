c0a719a1dfc52e1ef4ae6b54a39b69596a3d440c
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-20 00:00:25 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,32 @@
 def consolidate_cart(cart)
   # code here
+  newhash = Hash.new
+  cart.each do |value| 
+    #newitem = text.scan(/"([^"]*)"/)
+    if newhash[value.keys[0]]
+      newhash[value.keys[0]][:count]+=1 
+    else
+      newhash[value.keys[0]] = {
+        :price => value.values[0][:price],
+        :clearance => value.values[0][:clearance],
+        :count => 1
+      }
+    end
 end
+ return newhash
+end
+
 
 def apply_coupons(cart, coupons)
   # code here
+  coupon.each do |coupon|
+  if cart.keys.include?(coupon[:item])
+    if cart[coupon[:item]][:count] >= coupon[:count]
+      withCoupon = "#{coupon[:item]} W/COUPON"
+      if cart[withCoupon]
+        cart[withCoupon][:count]+=coupon[:num]
+    end
+  end
 end
 
 def apply_clearance(cart)
